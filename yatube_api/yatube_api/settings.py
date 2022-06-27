0da691f270c2b4fe0c0292130b89d9b4458dd639
Александр Иванченко <xKapellMeisterx@yandex.ru>
2022-06-27 19:18:40 +0300

    "Сдача проекта."

--- yatube_api/yatube_api/settings.py
+++ yatube_api/yatube_api/settings.py
@@ -99,8 +99,8 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-   'ACCESS_TOKEN_LIFETIME': timedelta(days=10),
-   'AUTH_HEADER_TYPES': ('Bearer',),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=10),
+    'AUTH_HEADER_TYPES': ('Bearer',),
 }
 
 EMPTY_VALUE_DISPLAY = '-пусто-'
