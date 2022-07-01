3dc97b67b3c0ab6b434561b9e2dbfdb55d97502a
Александр Иванченко <xKapellMeisterx@yandex.ru>
2022-07-01 16:36:54 +0300

    "Добален метод has_permissions в класс OwnerOrReadOnly."

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -2,6 +2,10 @@ from rest_framework import permissions
 
 
 class OwnerOrReadOnly(permissions.BasePermission):
+    def has_permissions(self, request, view):
+        return (request.method in permissions.SAFE_METHODS
+                or request.user.is_authenticated)
+
     def has_object_permission(self, request, view, obj):
         return (request.method in permissions.SAFE_METHODS
                 or obj.author == request.user)
